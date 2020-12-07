654b719d1e09610f31bd78a3f781cb3c95412221
Steven Hofmeyr <shofmeyr@lbl.gov>
2020-12-07 10:41:45 -0800

    fixed virion_plot.py to work with ODE results

--- scripts/virion_plot.py
+++ scripts/virion_plot.py
@@ -33,6 +33,7 @@ argparser.add_argument("--virus-scale", type=float, dest='virus_scale', default=
 argparser.add_argument("--chemo-scale", type=float, dest='chemo_scale', default=1.0, help='Factor to scale comparison chemokine levels')
 argparser.add_argument("--log", dest='log_scale', action="store_true", help='Use log scale for epicells and tcells')
 argparser.add_argument("-d", "--dimensions", required=True, help="Dimensions along one side")
+argparser.add_argument("-a", "--animate", action="store_true", help="Animate to see values changing as simcov.stats is update")
 options = argparser.parse_args()
 
 #columns = [int(i) for i in options.indexes.split(',')]
@@ -40,18 +41,21 @@ options = argparser.parse_args()
 fig = plt.figure(figsize=(12, 6))
 ax_epicells = fig.add_subplot(2, 2, 1)
 ax_tcells = fig.add_subplot(2, 2, 2)
-ax_virus = fig.add_subplot(2, 2, 3)
-ax_total_virions = fig.add_subplot(2, 2, 4)
+#ax_virus = fig.add_subplot(2, 2, 3)
+ax_total_virions = fig.add_subplot(2, 2, 3)
 
-moddate = os.stat('./simcov.stats')[8]
+moddate = os.stat(options.stats_file)[8]
 unchanged = 0
 first = True
 
 
-def plot_subplot(fname, ax, columns, title, lw=2, alpha=1.0, clear=True, log_scale=False, scale=1.0):
+def plot_subplot(fname, ax, columns, title, lw=2, alpha=1.0, clear=True, log_scale=False, scale=1.0, xs=None):
     graph_data = open(fname,'r').read()
     lines = graph_data.split('\n')
-    xs = []
+    get_xs = False
+    if xs == None:
+        get_xs = True
+        xs = []
     ys = []
     labels = []
     fields = lines[0][2:].split('\t')
@@ -64,7 +68,8 @@ def plot_subplot(fname, ax, columns, title, lw=2, alpha=1.0, clear=True, log_sca
         if line.startswith('#'):
             continue
         fields = line.split('\t')
-        xs.append(float(fields[0]) / options.resolution)
+        if get_xs:
+            xs.append(float(fields[0]) / options.resolution)
         for j in range(len(columns)):
             ys[j].append(scale * float(fields[columns[j]]))
     if clear:
@@ -82,6 +87,7 @@ def plot_subplot(fname, ax, columns, title, lw=2, alpha=1.0, clear=True, log_sca
     if log_scale:
         ax.set_yscale('log')
     plt.tight_layout()
+    return xs
 
 def animate(i):
     global moddate
@@ -102,25 +108,29 @@ def animate(i):
             #plot_subplot(options.compare_file, ax_tcells, [6, 7], 'tcells', lw=4, alpha=0.3, clear=False, log_scale=options.log_scale)
          #   plot_subplot(options.compare_file, ax_tcells, [6, 5], 'tcells', lw=4, alpha=0.3, clear=False, log_scale=options.log_scale)
             
-        plot_subplot(options.stats_file, ax_virus, [8], 'avg virions per cell')
+       # plot_subplot(options.stats_file, ax_virus, [8], 'avg virions per cell')
        # if options.compare_file != '':
             #plot_subplot(options.compare_file, ax_virus, [9], 'avg virions per cell', lw=4, alpha=0.3, clear=False, log_scale=False, scale=options.virus_scale)
          #   plot_subplot(options.compare_file, ax_virus, [8], 'avg virions per cell', lw=4, alpha=0.3, clear=False, log_scale=False, scale=options.virus_scale)
 
-        plot_subplot(options.stats_file, ax_total_virions, [8], 'virions', log_scale=options.log_scale, scale=int(options.dimensions) ** 2)
+        xs = plot_subplot(options.stats_file, ax_total_virions, [8], 'virions', log_scale=options.log_scale, scale=int(options.dimensions) ** 2)
         if options.compare_file != '':
-            plot_subplot(options.compare_file, ax_total_virions, [0], 'virions', lw=4, alpha=0.3, clear=False, log_scale=options.log_scale)
+            plot_subplot(options.compare_file, ax_total_virions, [0], 'virions', lw=4, alpha=0.3, clear=False, log_scale=options.log_scale, xs=xs, scale=options.virus_scale)
             
-    else:
-        unchanged += 1
-        if unchanged > 4:
-            moddate = new_moddate
-            unchanged = 0
-            plt.savefig(options.output + '.pdf')
-            plt.savefig(options.output + '.png')
+        if options.animate:
+            return
+
+    unchanged += 1
+    if not options.animate or unchanged > 4:
+        moddate = new_moddate
+        unchanged = 0
+        plt.savefig(options.output + '.pdf')
+        plt.savefig(options.output + '.png')
 
 
-    
-ani = animation.FuncAnimation(fig, animate, interval=1000)
+if options.animate:    
+    ani = animation.FuncAnimation(fig, animate, interval=1000)
+else:
+    animate(0)
 plt.show()
 
